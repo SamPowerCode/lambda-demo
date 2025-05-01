89aa63d577da70a4920437a89c93e0928d6b9fc0
Sam Power <samp25@gmail.com>
2025-05-01 14:12:50 +0100

    renamed folders

--- 01_never_do_this/never_do_this.py
+++ 01_never_do_this/never_do_this.py
@@ -8,3 +8,7 @@ result_lambda = add_lambda(1, 2)
 print(f"Function result: {result_func}")
 print(f"Lambda result: {result_lambda}")
 
+
+
+fancy_comp = {x: (lambda x: x ** 2)(x) for x in range(10)}
+print(f"Fancy comprehension: {fancy_comp}")
--- 06/never_do_this_either.py
+++
@@ -1,2 +0,0 @@
-fancy_comp = {x: (lambda x: x ** 2)(x) for x in range(10)}
-print(f"Fancy comprehension: {fancy_comp}")
